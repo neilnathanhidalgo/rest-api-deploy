32dc9770c75e47360a2de28d8d6513fc8050531c
Nathan Hidalgo <nathan_hidalgo@hotmail.com>
2024-03-04 16:02:10 -0500

    Deply api to FL0

---
+++ app.js
@@ -0,0 +1,129 @@
+const express = require("express");
+const crypto = require("node:crypto");
+const movies = require("./movies.json");
+const { validateMovie, validatePartialMovie } = require("./schemas/movies");
+
+const app = express();
+app.use(express.json());
+app.disable("x-powered-by");
+
+const ACCEPTED_ORIGINS = [
+  "http://localhost:8080",
+  "http://localhost:3001",
+  "https://movies.com",
+];
+
+//metodos normales: GET/HEAD/POST
+//metodos complejos: PUT/DELETE/PATCH => CORS PRE-Flight OPTIONS
+
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to the home page! :D<h1>");
+  //res.json({ message: "Welcome to the home page! :D" });
+});
+
+app.get("/movies", (req, res) => {
+  /*
+  const origin = req.header('origin');
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+
+*/
+  //Para el error de CORS en general con "*"
+  res.header("Access-Control-Allow-Origin", "*");
+
+  const { genre } = req.query;
+  if (genre) {
+    const filteredMovies = movies.filter((movie) =>
+      movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
+    );
+    return res.json(filteredMovies);
+  }
+  res.json(movies);
+});
+
+app.get("/movies/:id", (req, res) => {
+  const { id } = req.params;
+  const movie = movies.find((movie) => movie.id === id);
+
+  if (movie) return res.json(movie);
+
+  res.status(404).json({ message: "Movie not found" });
+});
+
+app.post("/movies", (req, res) => {
+  const result = validateMovie(req.body);
+
+  if (result.error) {
+    return res.status(400).json({ message: JSON.parse(result.error.message) });
+  }
+
+  const { title, genre, year, director, duration, poster } = req.body;
+  const newMovie = {
+    id: crypto.randomUUID(),
+    ...result.data,
+  };
+
+  movies.push(newMovie);
+
+  res.status(201).json(newMovie);
+});
+
+app.delete("/movies/:id", (req, res) => {
+  const origin = req.header("origin");
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+
+  const { id } = req.params;
+
+  const movieIndex = movies.findIndex((movie) => movie.id === id);
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: "Movie not found" });
+  }
+
+  movies.splice(movieIndex, 1);
+
+  return res.json({ message: "Movie deleted" });
+});
+
+app.patch("/movies/:id", (req, res) => {
+  const result = validatePartialMovie(req.body);
+
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) });
+  }
+  const { id } = req.params;
+  const movieIndex = movies.findIndex((movie) => movie.id === id);
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: "Movie not found" });
+  }
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data,
+  };
+
+  movies[movieIndex] = updateMovie;
+
+  return res.json(updateMovie);
+});
+
+app.options("/movies/:id", (req, res) => {
+  const origin = req.header("origin");
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  }
+
+  res.sendStatus(200);
+});
+
+const PORT = process.env.PORT ?? 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port http://localhost:${PORT}`);
+});
---
+++ schemas/movies.js
@@ -0,0 +1,43 @@
+const z = require("zod");
+
+const movieSchema = z.object({
+  title: z.string({
+    required_error: "Title is required",
+    invalid_type_error: "Title must be a string",
+  }),
+  genre: z.array(
+    z.enum([
+      "Action",
+      "Crime",
+      "Drama",
+      "Adventure",
+      "Sci-Fi",
+      "Animation",
+      "Romance",
+      "Biography",
+      "Fantasy",
+    ]),
+    {
+      required_error: "Genre is required",
+    }
+  ),
+  year: z.number().int().positive().min(1888).max(2024),
+  director: z.string(),
+  duration: z.number().int().positive(),
+  poster: z.string().url({
+    message: "Poster must be a valid URL",
+  }),
+});
+
+function validateMovie(input) {
+  return movieSchema.safeParse(input);
+}
+
+function validatePartialMovie(input) {
+  return movieSchema.partial().safeParse(input);
+}
+
+module.exports = {
+  validateMovie,
+  validatePartialMovie,
+};
